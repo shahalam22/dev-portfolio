945496b5bdadd7d3c3989a9f2e605519d2513320
SAAbir <shahalamabir21@gmail.com>
2024-06-25 09:25:06 +0600

    final version of personal portfolio

---
+++ components/Footer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import MagicButton from './MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
+import { socialMedia } from '@/data'
+
+
+const Footer = () => {
+    return (
+        <footer className="w-full mb-[100px] md:mb-5 pb-10" id="contact">
+            <div className="flex flex-col items-center">
+                <h1 className="heading lg:max-w-[45vw]">
+                    Ready to take <span className="text-purple">your</span> digital presence to the next level?
+                </h1>
+                <p className="text-white-200 md:mt-10 my-5 text-center">Reach out to me today and let's discuss how can I help you to achieve your goals.</p>
+                <a href="mailto:shahalamabir21@gmail.com">
+                    <MagicButton
+                        title = "Let's get in touch"
+                        icon = {<FaLocationArrow />}
+                        position = "right"
+                    />
+                </a>
+            </div>
+
+            <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+                <p className="text-white-200 md:font-normal font:light md:text-base text-sm">© 2024 Abir Shah Alam. All rights reserved.</p>
+            </div>
+
+            <div className="flex items-center md:gap-3 gap-6">
+                {socialMedia.map((profile) => (
+                    <div key={profile.id} className="w-10 h-10 cursor-pointer flex items-center justify-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+                        <img src={profile.img} alt="socmed" width={20} height={20}/>
+                    </div>
+                ))}
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,13 @@
+"use client";
+
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
+import { useState } from "react";
+import Lottie from "react-lottie";
+import animationData from "@/data/confetti.json";
+import MagicButton from "../MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
   className,
@@ -39,10 +47,20 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText("shahalamabir21@gmail.com");
+
+    setCopied(true);
+  };
+
+
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        "row-span-1 relative text-white overflow-hidden rounded-3xl border group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border-white/[0.1]",
         className
       )}
       style={{
@@ -50,7 +68,7 @@ export const BentoGridItem = ({
         backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(59,59,68,1) 26%, rgba(93,108,111,1) 100%)',
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
         <div className="w-full h-full absolute">
             {img && (
                 <img src={img} alt={img} className={cn(imgClassName, 'object-cover object-center')}/>
@@ -63,7 +81,7 @@ export const BentoGridItem = ({
         </div>
         {id === 6 && (
             <BackgroundGradientAnimation>
-                <div className="absolute z-50 flex items-center justify-center text-white font-bold"></div>
+              <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
             </BackgroundGradientAnimation>
         )}
         <div className={cn(
@@ -75,6 +93,48 @@ export const BentoGridItem = ({
             <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
               {title}
             </div>
+
+          {id === 2 && <GlobeDemo/>}
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {['React.js', 'Next.js', 'Typescript'].map((item) => (
+                  <span key={item} className="py-2 lg:py-4 px-3 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
+              </div>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
+                {['Tailwind', 'Node.js', 'MongoDB'].map((item) => (
+                  <span key={item} className="py-2 lg:py-4 px-3 lg:px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                <Lottie options={{
+                  loop: copied,
+                  autoplay: copied,
+                  animationData,
+                  rendererSettings: {
+                    preserveAspectRatio: 'xMidYMid slice'
+                  }
+                }}/>
+              </div>
+
+              <MagicButton 
+                title={copied ? "Email Copied" : "Copy My Email"} 
+                icon={<IoCopyOutline/>}
+                position="left"
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}  
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
